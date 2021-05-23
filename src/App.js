f911f24f5af511654714da320ac48d10061bc40b
Ahmed Anwar <aanwar@sumerge.com>
2021-05-23 23:00:38 +0200

    Change to React portfolio built from scratch

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.scss';
+import MainPage from './components/MainPage';
+
+const App = () =>
+    <div>
+        <header>
+            <MainPage/>
+        </header>
+    </div>
+
+export default App;
